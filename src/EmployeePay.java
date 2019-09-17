6eac1cf27d2787db3a7eed37e39709394d872125
marc6250 <marc6250@ISS2V0SKH2.scdsb.pri>
2019-09-17 11:08:59 -0400

    Initial commit

---
+++ src/EmployeePay.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+
+public class EmployeePay {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int hours;
+    }
+    
+}

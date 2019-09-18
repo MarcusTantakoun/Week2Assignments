d6c27d7617a0ee4466303d0e051cd68f3e99793d
marc6250 <marc6250@ISS2V0SKH2.scdsb.pri>
2019-09-18 12:00:28 -0400

    Final change

--- src/PowersChart.java
+++ src/PowersChart.java
@@ -1,11 +1,26 @@
 
 import java.util.Scanner;
 
-
 public class PowersChart {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        int exp;
+        while (true) {
+            System.out.print("Enter exponent value: ");
+            exp = s.nextInt();
+            if (exp >= 0 && exp <= 10) {
+                break;
+            }
+            System.out.println("Error, number must be from 0 to 10");
+        }
+        
+        System.out.format("%d\t",exp); System.out.format("2^%d\n",exp);
+        System.out.println("-----------\t");
+        
+        for (int i = 0; i < exp; i++) {
+            System.out.format("%d\t",i+1);
+            System.out.println(Math.pow(2,i+1));
+        }
     }
-    
 }

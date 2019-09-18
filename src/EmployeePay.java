15d55b50adc3ee56dd2fc0e9fe45e83ebdb7fa27
marc6250 <marc6250@ISS2V0SKH2.scdsb.pri>
2019-09-18 08:56:36 -0400

    Final change

--- src/EmployeePay.java
+++ src/EmployeePay.java
@@ -1,12 +1,34 @@
 
 import java.util.Scanner;
 
-
 public class EmployeePay {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int hours;
+        double pay=0, overtime=0;
+
+        for (int i = 1; i < 5; i++) {
+            while (true) {
+                System.out.print("Enter hours worked for Employee " + (i * 1) + ": ");
+                hours = s.nextInt();
+                if (hours >= 1 && hours <= 70) {
+                    break;
+                } 
+                System.out.println("Invalid, try again");
+            }
+            if(hours>40){
+                pay = 600;
+                hours -=40;
+                overtime = hours*22.50;
+            }
+            else if(hours<=40){
+                pay = hours*15.00;
+            }
+            System.out.format("Regular Pay: $%.2f\n", pay);
+            System.out.format("Overtime Pay: $%.2f\n", overtime);
+            System.out.println("----------------------");
+        }
     }
-    
+
 }

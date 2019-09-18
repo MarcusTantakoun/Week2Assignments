c9ba8fe2666c4473ed7da5c2a221fb27b24eab6b
marc6250 <marc6250@ISS2V0SKH2.scdsb.pri>
2019-09-18 11:07:37 -0400

    Final change

--- src/Divide.java
+++ src/Divide.java
@@ -1,11 +1,26 @@
 
 import java.util.Scanner;
 
-
 public class Divide {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        int num1, num2, total;
+
+        System.out.print("Enter number 1: ");
+        num1 = s.nextInt();
+        System.out.print("Enter number 2: ");
+        num2 = s.nextInt();
+        
+        if(num1 > num2){
+            total = num1/num2;
+            int remain1 = num1%num2;
+            System.out.format("%d / %d will be %d and the remainder will be %d\n", num1, num2, total, remain1);
+        }
+        else if(num2>num1){
+            total = num2/num1;
+            int remain2 = num2%num1;
+            System.out.format("%d / %d will be %d and the remainder will be %d\n", num2, num1, total, remain2);
+        }
     }
-    
 }

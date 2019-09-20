11afd818431c9d91399b3206324de63df4894487
marc6250 <marc6250@ISS2V0SKH2.scdsb.pri>
2019-09-20 11:57:27 -0400

    Final change

--- src/SalaryTable.java
+++ src/SalaryTable.java
@@ -2,6 +2,18 @@
 public class SalaryTable {
 
     public static void main(String[] args) {
+        double oldsal = 0, raise=0, newsal=40000;
+
+        System.out.format("%s%22s%18s%23s\n", "YEAR", "OLD SALARY", "RAISE", "NEW SALARY");
+        System.out.format("%s%22s%18s%23s\n", "----", "----------", "-----", "----------");
+        System.out.println(" 1\t\t   $---\t\t      $40000\t\t   $40000");
+        
+        for (int years = 2; years <= 10; years++) {
+            raise = newsal*0.03;
+            System.out.format("%2d%22.2f%20.2f%23.2f\n",years,oldsal,raise,newsal);
+            newsal = raise+newsal;
+            oldsal = newsal;
+        }
         
     }
     
